869a7cde4a664cdd032a4e8b5087486fafd71346
Jayant_135 <jayantgaur28@gmail.com>
2022-12-02 09:16:01 +0530

    final commit

--- components/header/index.js
+++ components/header/index.js
@@ -2,24 +2,23 @@ import Button from "../button";
 import styles from "./header.module.css";
 
 export default function Header({ handleConnectWallet, headerLabel }) {
-    return (
-        <div className={styles["header"]}>
-            <div className={styles["left"]}>
-                <div className={styles["logo-title"]}>
-                    <img src={"/logo.svg"} />
-                    <span>
-                        <span style={{ color: "#8D72E1" }}>De-Fi</span>le Drive
-                    </span>
-                </div>
-                <p>
-                    A GoogleDrive/Dropbox for files but decentralised powered
-                    with e2e file encryption, private group creation for your
-                    friends and file sharing with no link management hassle!
-                </p>
-            </div>
-            <div className={styles["right"]}>
-                <Button label={headerLabel} onPress={handleConnectWallet} />
-            </div>
+  return (
+    <div className={styles["header"]}>
+      <div className={styles["left"]}>
+        <div className={styles["logo-title"]}>
+          <span>
+            <span style={{ color: "#8D72E1" }}>De-Fi</span>le Drive
+          </span>
         </div>
-    );
+        <p>
+          A GoogleDrive/Dropbox for files but decentralised powered with e2e
+          file encryption, private group creation for your friends and file
+          sharing with no link management hassle!
+        </p>
+      </div>
+      <div className={styles["right"]}>
+        <Button label={headerLabel} onPress={handleConnectWallet} />
+      </div>
+    </div>
+  );
 }
